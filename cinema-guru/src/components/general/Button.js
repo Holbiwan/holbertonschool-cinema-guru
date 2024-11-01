6673b1715212927c7fa6be4ddfbb4f41738c3401
Holbiwan <sabrinapapeau@gmail.com>
2024-11-01 09:58:49 +0100

    Update code

--- cinema-guru/src/components/general/Button.js
+++ cinema-guru/src/components/general/Button.js
@@ -2,16 +2,13 @@ import React from 'react';
 import './general.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function Button({
-  text,
-  className,
-  onClick,
-  icon,
-}) {
+const Button = ({ label, type = "button", className = "", onClick, icon }) => {
   return (
-    <button className={className} onClick={onClick}>
+    <button type={type} className={`btn ${className}`} onClick={onClick}>
       {icon && <FontAwesomeIcon icon={icon} />}
-      {text}
+      {label}
     </button>
   );
-}
+};
+
+export default Button;
